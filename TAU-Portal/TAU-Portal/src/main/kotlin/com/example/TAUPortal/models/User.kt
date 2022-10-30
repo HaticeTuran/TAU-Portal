a4af4f09e65a676f7600b3d765ceaa36cde9ecf9
Hatice <e190503011@stud.tau.edu.tr>
2022-10-30 22:50:02 +0300

    added user class

---
+++ TAU-Portal/TAU-Portal/src/main/kotlin/com/example/TAUPortal/models/User.kt
@@ -0,0 +1,9 @@
+data class User(
+        val user_id: UUID,
+        val name: String,
+        val surname: String,
+        var mail: String,
+        var password: String,
+        var tel_no: String,
+        var role: String
+);
