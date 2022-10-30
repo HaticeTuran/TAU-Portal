2a35843d082518b5e6133900e6fef0c10bf185f3
Hatice <e190503011@stud.tau.edu.tr>
2022-10-30 22:16:48 +0300

    First Commit

---
+++ TAU-Portal/TAU-Portal/src/main/kotlin/com/example/TAUPortal/TauPortalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.TAUPortal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TauPortalApplication
+
+fun main(args: Array<String>) {
+	runApplication<TauPortalApplication>(*args)
+}
---
+++ TAU-Portal/TAU-Portal/src/test/kotlin/com/example/TAUPortal/TauPortalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.TAUPortal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TauPortalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
